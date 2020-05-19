8eca2549c1285bd8f9e181832555a7351b8985b4
Deborah Nakakande <d.nakakande@hotmail.com>
2020-05-19 15:34:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function deleteFromObjectByKey(object, key){
-  var newObj = Object.assign({}, object)
-  delete newObj.key
-  return object
+function deleteFromObjectByKey(obj, key){
+  delete Object.assign({}, {obj: key})
+  return obj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key;
-  return object
+
 }
